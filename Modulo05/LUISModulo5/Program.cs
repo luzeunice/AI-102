a09a1def8d78799869e5e19d072ac1ebff9dabf9
Luz Eunice <luzeunice@tec.mx>
2021-06-23 19:32:20 +0000

    ok

---
+++ Modulo05/LUISModulo5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime;
+using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime.Models;
+using LUISModulo5.luis;
+using Newtonsoft.Json.Linq;
+
+namespace LUISModulo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cliente = LuisClient.Cliente;
+            Console.WriteLine("-.-.-.-.Prueba de LUIS en C#-.-.-.-.-.");
+            Console.WriteLine("Escribe una intención: ");
+            var mensaje = Console.ReadLine();
+            GetPrediction(cliente, mensaje);
+            Console.ReadKey();
+            
+
+            
+        }
+
+        private async static void GetPrediction(LUISRuntimeClient cliente, string mensaje) {
+            var kbLUIS = "30018600-f54e-46ce-a650-954172c06484";
+            var request = new PredictionRequest { 
+              Query= mensaje
+            };
+            var result = await cliente.Prediction.GetSlotPredictionAsync(Guid.Parse(kbLUIS), slotName:"Staging", request);
+            var prediction = result.Prediction;
+            switch (prediction.TopIntent) {
+                case "EnviaCorreo":
+                    Console.WriteLine("La intención es enviar correo");
+                    var entities = prediction.Entities;
+                    var correo = entities["Correo"];
+                    Console.WriteLine($"Enviando correo a {((JArray)correo)[0]}");
+                    break;
+                default:
+                    Console.WriteLine("No se cuál es la intención del usuario");
+                    break;
+            }
+
+        }
+
+
+    }
+}
---
+++ Modulo05/LUISModulo5/luis/LuisClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime;
+
+namespace LUISModulo5.luis
+{
+    class LuisClient
+    {
+        private static readonly string API_KEY = "3882d2f2b4f1498ca6312471721d0a00";
+        private static readonly string ENDPOINT = "https://westus.api.cognitive.microsoft.com/";
+
+        public static LUISRuntimeClient Cliente { get; private set; }
+
+        static LuisClient() { InitLuis(); }
+        private static void InitLuis() {
+            if (Cliente==null) {
+                var credentials = new ApiKeyServiceClientCredentials(API_KEY);
+                Cliente = new LUISRuntimeClient(credentials) { Endpoint=ENDPOINT};
+            }
+        }
+
+    }
+}
