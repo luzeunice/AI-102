271bb4c5ef4631d40e04288e19da76ff554ee9a0
Luz Eunice <luzeunice@tec.mx>
2021-06-24 19:32:44 +0000

    Actualizar

---
+++ Modulo08/ComputerModulo8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using System.Linq;
+using ComputerModulo8.computer;
+using System.IO;
+namespace ComputerModulo8
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cliente = ComputerClient.clienteComputer;
+            Console.WriteLine("Escribe url de imagen local: ");
+            var url = Console.ReadLine();
+            DescribeImage(cliente, url);
+
+            Console.ReadKey();
+           
+        }
+
+        private async static void DescribeImage(ComputerVisionClient cliente, String url) {
+            var imgStream = new FileStream(url, FileMode.Open, FileAccess.Read, FileShare.Read);
+            var result = await cliente.DescribeImageInStreamAsync(imgStream, language:"es");
+            var captions = result.Captions;
+            var tags = result.Tags;
+            Console.WriteLine("-.-.-.Descripción general de la imagen-.-.-.");
+            Console.WriteLine($"Descripción de imagen: {captions[0].Text}");
+            Console.WriteLine($"Score: {captions[0].Confidence}");
+            Console.WriteLine($"-.-.-.Etiquetas.--.-.-.-.");
+            tags.ToList().ForEach(t => Console.WriteLine($"tag: {t}")) ;
+            
+        
+        }
+    }
+}
---
+++ Modulo08/ComputerModulo8/computer/ComputerClient.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputerModulo8.computer
+{
+    class ComputerClient
+    {
+        public static ComputerVisionClient clienteComputer { get; set; }
+        private static string endpoint = "https://computerenvef.cognitiveservices.azure.com/";
+        private static string apiKey = "c335ea0982e543719821285136e137f4";
+        static ComputerClient()
+        {
+            Console.WriteLine("inicializando Cliente Computer");
+            InicializeComputerClient();
+        }
+
+        private static void InicializeComputerClient()
+        {
+            var credentials = new ApiKeyServiceClientCredentials(apiKey);
+            clienteComputer = new ComputerVisionClient(credentials)
+            { Endpoint = endpoint };
+        }
+
+    }
+}
